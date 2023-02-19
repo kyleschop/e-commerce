74a8d4d7365b017b136e21c34732e67d6c8f4c14
Kyle Schoppmann <klparuu@yahoo.com>
2023-02-19 12:18:02 -0700

    develope index.js file

--- models/index.js
+++ models/index.js
@@ -6,13 +6,23 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  
-})
+  foreignKey: 'category_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(ProductTag, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
